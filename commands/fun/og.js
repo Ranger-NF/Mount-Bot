6791e63d4fbc3b80951e33b5a390d47c8504c3e0
Ranger <yemzero0@gmail.com>
2021-06-04 22:53:22 +0530

    🖼️ Changed og command's overlay

--- commands/fun/og.js
+++ commands/fun/og.js
@@ -2,6 +2,7 @@ const {Command} = require('discord.js-commando');
 const discord = require('discord.js');
 const math = require('../../helpers/math.js');
 const jimp = require('jimp');
+const { greyscale } = require('jimp');
 
 module.exports = class ogCommand extends Command {
 	constructor(client){
@@ -25,18 +26,20 @@ module.exports = class ogCommand extends Command {
 		const joinedTimeStamp = `${convertedTime.days} days and ${convertedTime.hours} hours!`
 
 		const avatar = ogMember.user.avatarURL({format:'png'})
-		const background = await jimp.read(avatar)
-		const crown = await jimp.read('./overlays/crown.png')
-		background.composite(crown, 0 , 0)
-		background.write('./manipulated/og.png')
+		let background = await jimp.read(avatar)
+		let greyscale_bg = background.grayscale();
+		const blackNWhite = greyscale_bg.contrast(0);
+		const crown = await jimp.read('./overlays/og_badge.png')
+		blackNWhite.composite(crown, 0 , 0)
+		blackNWhite.write('./manipulated/og.png')
 
 		const attachment = new discord.MessageAttachment('./manipulated/og.png', 'og.png');
 
 		let ogEmbed = new discord.MessageEmbed()
 			.setColor('WHITE')
 			.setTitle('OG scale')
-			.setDescription(`${ogMember.user.username} is more cooler than ${nonOg.user.username}`)
-			.addField('They have a difference of more than', joinedTimeStamp)
+			.setDescription(`${ogMember.user.username} is cooler than ${nonOg.user.username}`)
+			.addField(`They have a difference of more than ${joinedTimeStamp}!`, '\u200B')
 			.attachFiles(attachment)
 			.setImage('attachment://og.png')
 			.setFooter(`Command invoked by ${message.author.tag}`, message.author.avatarURL());
